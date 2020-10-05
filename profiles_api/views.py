1646e77943b92c5f1ebefe3a30acf7545a09e2ef
atbee <atb@odds.team>
2020-10-06 00:12:02 +0700

    Create serializer for POST hello view :bee:

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 
 class HelloAPIView(APIView):
+
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         an_apiview = [
@@ -13,3 +19,17 @@ class HelloAPIView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        serializers = self.serializer_class(data=request.data)
+
+        if serializers.is_valid():
+            name = serializers.validated_data.get('name')
+            message = f'Hello {name}'
+
+            return Response({'message': message})
+        else:
+            return Response(
+                serializers.errors,
+                status=status.HTTP_400_BAD_REQUEST,
+            )
